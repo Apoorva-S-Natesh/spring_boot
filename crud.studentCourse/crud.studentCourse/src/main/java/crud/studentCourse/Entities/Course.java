edda3b13cde1c99ec7d9a599a2c9504f278e89af
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-05-26 18:33:15 +0200

    feat: add entities student and course

--- crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Entities/Course.java
+++ crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Entities/Course.java
@@ -1,5 +1,73 @@
 package crud.studentCourse.Entities;
 
-public class Course {
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Course {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String courseName;
+	private String description;
+	
+	@ManyToMany(mappedBy = "courses")
+	private List<Student> students = new ArrayList<>();
+
+	public Course() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Course(long id, String courseName, String description, List<Student> students) {
+		super();
+		this.id = id;
+		this.courseName = courseName;
+		this.description = description;
+		this.students = students;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", courseName=" + courseName + ", description=" + description + ", students="
+				+ students + "]";
+	}
 }
--- crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Entities/Student.java
+++ crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Entities/Student.java
@@ -1,5 +1,72 @@
 package crud.studentCourse.Entities;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Student {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	private String email;
+	
+	@ManyToMany
+	private List <Course> courses = new ArrayList<>();
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(long id, String name, String email, List<Course> courses) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.courses = courses;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", courses=" + courses + "]";
+	}
 }
