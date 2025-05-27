c3cf4bf66a827ec5854747dc8997747205cc6b95
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-05-27 16:35:40 +0200

    feat: add Course services

--- crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Services/CourseService.java
+++ crud.studentCourse/crud.studentCourse/src/main/java/crud/studentCourse/Services/CourseService.java
@@ -1,6 +1,30 @@
 package crud.studentCourse.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import crud.studentCourse.Entities.Course;
+import crud.studentCourse.Repositories.courseRepository;
+
 @Service
 public class CourseService {
+	@Autowired private courseRepository repo;
+	
+	 public List<Course> findAll() {
+	        return repo.findAll();
+	    }
 
+	    public Course findById(Long id) {
+	        return repo.findById(id).orElse(null);
+	    }
+
+	    public void save(Course course) {
+	        repo.save(course);
+	    }
+
+	    public void delete(Long id) {
+	        repo.deleteById(id);
+	    }
 }
